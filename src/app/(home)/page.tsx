eb75a58c833adda0c52dd34e74e9831696056069
Arthur <arthur.fabricyo@gmail.com>
2026-06-22 12:52:22 -0200

    fix: log NASA S2S encryption key for debugging purposes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./_components/hero-section";
 
 export default function Home() {
+  console.log(process.env.NASA_S2S_ENCRYPTION_KEY);
   return (
     <div className="relative">
       <HeroSection />
