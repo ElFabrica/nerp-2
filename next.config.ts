be6102f5e57ab8a54848e6e8a3d2b67fb562900b
Arthur <arthur.fabricyo@gmail.com>
2026-06-23 23:33:01 -0200

    fix: remove S3 bucket hostname configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
-const s3BucketHostname = process.env.NEXT_PUBLIC_S3_BUCKET_CONSTRUCTOR_URL;
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
         hostname: "erp-limas-app.t3.storage.dev",
-        port: "",
-        protocol: "https",
-      },
-      {
-        hostname: process.env.NEXT_PUBLIC_S3_BUCKET_CONSTRUCTOR_URL ?? "",
         port: "",
         protocol: "https",
       },
